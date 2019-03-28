9bd24bb78c2260f6e30c8aaf6c4b8e00def64eb4
student <student@labstudenti.finki.ukim.mk>
2019-03-28 10:52:51 +0100

    Initial commit

---
+++ program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	cout << "Andrej Kocev" << endl;
+}
+
+void printMyIndex () {
+	//TODO
+}
+
+int main () {
+	//TODO
+}
